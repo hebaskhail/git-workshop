c5135878c966216f2f6ff7089ff6d947d14d5e2f
Mohammed Tamous <44541268+mrtamous@users.noreply.github.com>
2023-03-05 14:17:06 +0200

    add test

--- __tests__/Multiplication.js
+++ __tests__/Multiplication.js
@@ -1,6 +1,19 @@
-const Multiplication =  require('../src/Multiplication');
+const Multiplication = require("../src/Multiplication");
 
+test("Multiplication any number in 0 will be 0", () => {
+  expect(Multiplication(1, 0)).toBe(0);
+});
 
-const Multiplication = (firstNumber, secondNumber) => {
-  return firstNumber * secondNumber;
-};
+test("Multiplication any number in 1 will be other number", () => {
+  expect(Multiplication(10, 1)).toBe(10);
+});
+
+describe("Multiplication", () => {
+  test("Multiplication any number in 0 will be 0", () => {
+    expect(Multiplication(1, 0)).toBe(0);
+  });
+
+  test("Multiplication any number in 1 will be other number", () => {
+    expect(Multiplication(10, 1)).toBe(10);
+  });
+});
